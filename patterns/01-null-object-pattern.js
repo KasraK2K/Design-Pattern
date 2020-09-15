da4797fb94821283a7c9a1fde17728ad091f5d9e
Kasra Karami <Kasra_K2K@yahoo.com>
2020-09-15 09:47:27 +0430

    init project and create null object pattern

---
+++ patterns/01-null-object-pattern.js
@@ -0,0 +1,43 @@
+class User {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
+  hasAccess() {
+    return this.name === "Bob";
+  }
+}
+
+class NullUser {
+  constructor() {
+    this.id = -1;
+    this.name = "Guest";
+  }
+  hasAccess() {
+    return false;
+  }
+}
+
+const users = [new User(1, "Bob"), new User(2, "John")];
+
+function getUser(id) {
+  const user = users.find((user) => user.id === id);
+  if (user == null) return new NullUser();
+  else return user;
+}
+
+function printUser(id) {
+  const user = getUser(id);
+
+  let name = "Guest";
+  if (user != null && user.name != null) name = user.name;
+  console.log("Hello " + name);
+
+  if (user != null && user.hasAccess != null && user.hasAccess() != null) {
+    console.log("You have access");
+  } else {
+    console.log("You are not allowed here");
+  }
+}
+
+printUser(3);
